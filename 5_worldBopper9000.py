26c3aeb5f085b55800cbc37887464a9f3bef31ed
pjagada <pranavj918@gmail.com>
2022-06-10 16:21:17 -0500

    rearrange files and add worldBopper9000

---
+++ 5_worldBopper9000.py
@@ -0,0 +1,15 @@
+import shutil
+import glob
+import os
+
+# Edit this
+INST_FOLDER = "C:/MultiInstanceMC/MultiMC/instances"
+
+with open("bop.log", "w") as log:
+    files = glob.glob(f'{INST_FOLDER}/*/.minecraft/saves/*')
+    for f in files:
+        if os.path.isdir(f) and ("New World" in f or "Speedrun #" in f):
+            log.write("Deleting: " + f + "\n")
+            shutil.rmtree(f)
+        else:
+            log.write("Skipping: " + f + "\n")
